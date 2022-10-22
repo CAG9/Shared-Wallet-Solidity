6097e8397c142ef4ee3ed41aae9296b3caf23a3e
Cesar Arcos-Gonzalez <60860385+CAG9@users.noreply.github.com>
2022-10-22 15:03:19 -0500

    Update SharedWallet.sol

--- SharedWallet.sol
+++ SharedWallet.sol
@@ -1,14 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SharedWallet is Ownable {
+contract SharedWallet {
+    
+    event AllowanceChanged(address indexed _forWho,address indexed _fromWhom,  uint _oldAmount, uint _newAmount);
+    
+    address public owner;
+    mapping(address => uint) public allowance;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(owner == msg.sender, "You are not allowed");
+        _;
+    }
+
+    function isOwner() public view returns(bool){
+        if(owner == msg.sender){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    modifier ownerOrAllowed(uint _amount) {
+        require(isOwner() || allowance[msg.sender] >= _amount,"You are not allowed");
+        _;
+
+    }
+
+    function reduceAllowance(address _who, uint _amount) internal {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], allowance[_who] - _amount);
+        allowance[_who] -= _amount;
+    }
+
+
+    function addAllowance(address _who, uint _amount) public onlyOwner {
+        emit AllowanceChanged(_who, msg.sender, allowance[_who], _amount);
+        allowance[_who] = _amount;
+    }
 
     
-    function withdrawMoney(address payable _to, uint _amount) public onlyOwner {
+    function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount) {
+        require(_amount <= address(this).balance, "There are not enough funds stored in the smart contract");
+        if (!isOwner()){
+            reduceAllowance(msg.sender, _amount);
+        }
         _to.transfer(_amount);
     }
 
     receive() external payable{}
+
+
+
+
+
 }
